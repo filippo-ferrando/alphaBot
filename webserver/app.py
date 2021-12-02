04ef19b61e86be640dda74cab33d1e8bc9e23999
filippo-ferrando <filippo.ferrando@itiscuneo.eu>
2021-12-02 08:56:18 +0100

    basic webserver implementation

---
+++ webserver/app.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template, request
+import time
+import RPi.GPIO as GPIO, subprocess
+app = Flask(__name__)
+
+class AlphaBot(object):
+    
+    def __init__(self, in1=13, in2=12, ena=6, in3=21, in4=20, enb=26):
+        self.IN1 = in1
+        self.IN2 = in2
+        self.IN3 = in3
+        self.IN4 = in4
+        self.ENA = ena
+        self.ENB = enb
+        self.PA  = 50
+        self.PB  = 50
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.IN1, GPIO.OUT)
+        GPIO.setup(self.IN2, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        GPIO.setup(self.ENA, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        self.PWMA = GPIO.PWM(self.ENA,500)
+        self.PWMB = GPIO.PWM(self.ENB,500)
+        self.PWMA.start(self.PA)
+        self.PWMB.start(self.PB)
+        self.stop()
+
+    def left(self, sTime=2, speed=30):  #funzione per "girare" a sinistra, definiamo noi la velocità e il tempo di eseguzione della funzione (questo vale per tutte le funzioni di movimento)
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+        time.sleep(sTime)
+        self.stop()
+        
+    def stop(self):
+        self.PWMA.ChangeDutyCycle(0)
+        self.PWMB.ChangeDutyCycle(0)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.LOW)
+        
+    def right(self, sTime=2, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+        time.sleep(sTime)
+        self.stop()
+
+    def forward(self, sTime=2, speed=40):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)
+        time.sleep(sTime)
+        self.stop()
+
+    def backward(self, sTime=2, speed=40):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)
+        time.sleep(sTime)
+        self.stop()
+        
+    def set_pwm_a(self, value):
+        self.PA = value
+        self.PWMA.ChangeDutyCycle(self.PA)
+
+    def set_pwm_b(self, value):
+        self.PB = value
+        self.PWMB.ChangeDutyCycle(self.PB)    
+        
+    def set_motor(self, left, right):
+        if (right >= 0) and (right <= 100):
+            GPIO.output(self.IN1, GPIO.HIGH)
+            GPIO.output(self.IN2, GPIO.LOW)
+            self.PWMA.ChangeDutyCycle(right)
+        elif (right < 0) and (right >= -100):
+            GPIO.output(self.IN1, GPIO.LOW)
+            GPIO.output(self.IN2, GPIO.HIGH)
+            self.PWMA.ChangeDutyCycle(0 - right)
+        if (left >= 0) and (left <= 100):
+            GPIO.output(self.IN3, GPIO.HIGH)
+            GPIO.output(self.IN4, GPIO.LOW)
+            self.PWMB.ChangeDutyCycle(left)
+        elif (left < 0) and (left >= -100):
+            GPIO.output(self.IN3, GPIO.LOW)
+            GPIO.output(self.IN4, GPIO.HIGH)
+            self.PWMB.ChangeDutyCycle(0 - left)
+
+ab = AlphaBot()
+time = 0.5
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        #print(request.form.get('forward'))
+        if request.form.get('forward') == 'fw':
+            print("Avanti")
+            Ab.forward(sTime=time)
+        elif  request.form.get('backward') == 'bw':
+            print("Indietro")
+            ab.backward(sTime=time)
+        elif  request.form.get('left') == 'lt':
+            print("Sinistra")
+            ab.right(sTime=time)
+        elif  request.form.get('right') == 'rt':
+            print("Destra")
+            ab.right(sTime=time)
+        else:
+            print("Unknown")
+    elif request.method == 'GET':
+        return render_template('index.html')
+    
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost')

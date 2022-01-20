0128b2bcdb2778ef440328eff99f91fc15df8a50
filippoferrando <filippo.ferrando@itiscuneo.eu>
2022-01-20 10:04:13 +0100

    update

--- webserver/app.py
+++ webserver/app.py
@@ -3,6 +3,7 @@ from sqlite3 import Error
 from flask import Flask, render_template, request, redirect, url_for
 import time, random, string
 import RPi.GPIO as GPIO, subprocess
+from datetime import datetime
 
 app = Flask(__name__)
 token = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16))
@@ -21,6 +22,7 @@ def validate(username, password):
         if dbUser == username:
             completion = check_password(dbPass, password)
     return completion
+    db.close()
 
 def check_password(hashed_password, user_password):
     return hashed_password == user_password
@@ -35,13 +37,32 @@ def create_connection(db_file): #funzione per connettere il database allo script
     return conn
 
 def select_task_id(conn, id):   #returna w.1 oppure w.1;s.3 in base alla query scritta sul db
+    conn = create_connection("Movimenti.db")
     cur = conn.cursor()         #in pratica questo serve solamente a fare le query per fare il retrive della lista di comandi
     cur.execute(f"SELECT sequenza FROM Movimenti Where id = {id}") #esecuzione della query
 
-    rows = cur.fetchall() #applicazione della query sul database
+    rows = cur.fetchall()
 
     for row in rows:
-        return(row[0]) #retrive della tabella creata
+        return(row[0])
+
+    conn.close()
+
+def history(user, command):
+    conn = create_connection("Movimenti.db")
+    cur = conn.cursor()         #in pratica questo serve solamente a fare le query per fare il retrive della lista di comandi
+    cur.execute(f"INSERT INTO HISTORY (Utente, Command, Date) VALUES ('{user}', '{command}', '{datetime.today()}')") #esecuzione della query
+    cur.execute("commit")
+    conn.close()
+
+def login_log(user):
+    conn = create_connection("Movimenti.db")
+    date = datetime.today()
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO LOG_UTENTI (USERNAME, DATE) VALUES ('{user}', '{date}')")
+    cur.execute("commit")
+    conn.close()
+    
 
 class AlphaBot(object):
 
@@ -149,6 +170,7 @@ ab = AlphaBot()
 dtime = 0.5
 
 mDict = {"forward":1,"backward":2, "left":3, "right":4, "stop":5, "fb":6, "zigzag":7, "drift":8}
+username = ''
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -163,6 +185,7 @@ def login():
             error = 'Invalid Credentials. Please try again.'
             print(error)
         else:
+            login_log(username)
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
@@ -178,20 +201,28 @@ def index():
 
     if request.method == 'POST':
         #print(request.form.get('forward'))
+
         if request.form.get('forward') == '▲':
+            #print(username, 'forward')
+            history(username, 'forward')
             print("Avanti")
             ab.forward(sTime=dtime)
         elif  request.form.get('backward') == '▼':
+            history(username, 'backward')
             print("Indietro")
             ab.backward(sTime=dtime)
         elif  request.form.get('left') == '◄':
+            history(username, 'left')
             print("Sinistra")
             ab.left(sTime=dtime)
         elif  request.form.get('right') == '►':
+            history(username, 'right')
             print("Destra")
             ab.right(sTime=dtime)
         elif request.form.get('movement'):
             data = request.form.get('movement').lower()
+            print(username, data)
+            history(username, data)
             commandList = select_task_id(connDb, mDict[data]).split(";")
             print(commandList)
             for command in commandList:

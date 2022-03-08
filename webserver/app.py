bf420a187288e1b7efc1d910c265841a47e21638
filippoferrando <filippo.ferrando@itiscuneo.eu>
2022-03-08 11:34:27 +0100

    hardened login

--- webserver/app.py
+++ webserver/app.py
@@ -2,7 +2,7 @@ import sqlite3
 from sqlite3 import Error
 from flask import Flask, render_template, request, redirect, url_for, make_response
 import time, random, string
-import RPi.GPIO as GPIO, subprocess
+from alphabot import AlphaBot as ab
 from datetime import datetime
 
 app = Flask(__name__)
@@ -61,139 +61,80 @@ def login_log(user):
     cur.execute(f"INSERT INTO LOG_UTENTI (USERNAME, DATE) VALUES ('{user}', '{datetime.today()}')")
     cur.execute("commit")
     conn.close()
-    
-
-class AlphaBot(object):
-
-    def __init__(self, in1=13, in2=12, ena=6, in3=21, in4=20, enb=26):
-        self.IN1 = in1
-        self.IN2 = in2
-        self.IN3 = in3
-        self.IN4 = in4
-        self.ENA = ena
-        self.ENB = enb
-        self.PA  = 50
-        self.PB  = 50
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(self.IN1, GPIO.OUT)
-        GPIO.setup(self.IN2, GPIO.OUT)
-        GPIO.setup(self.IN3, GPIO.OUT)
-        GPIO.setup(self.IN4, GPIO.OUT)
-        GPIO.setup(self.ENA, GPIO.OUT)
-        GPIO.setup(self.ENB, GPIO.OUT)
-        self.PWMA = GPIO.PWM(self.ENA,500)
-        self.PWMB = GPIO.PWM(self.ENB,500)
-        self.PWMA.start(self.PA)
-        self.PWMB.start(self.PB)
-        self.stop()
-
-    def left(self, sTime=2, speed=30):  #funzione per "girare" a sinistra, definiamo noi la velocità e il tempo di eseguzione della funzione (questo vale per tutte le funzioni di movimento)
-        self.PWMA.ChangeDutyCycle(speed)
-        self.PWMB.ChangeDutyCycle(speed)
-        self.PWMA.ChangeDutyCycle(speed)
-        self.PWMB.ChangeDutyCycle(speed)
-        GPIO.output(self.IN1, GPIO.HIGH)
-        GPIO.output(self.IN2, GPIO.LOW)
-        GPIO.output(self.IN3, GPIO.HIGH)
-        GPIO.output(self.IN4, GPIO.LOW)
-        time.sleep(sTime)
-        self.stop()
-
-    def stop(self):
-        self.PWMA.ChangeDutyCycle(0)
-        self.PWMB.ChangeDutyCycle(0)
-        GPIO.output(self.IN1, GPIO.LOW)
-        GPIO.output(self.IN2, GPIO.LOW)
-        GPIO.output(self.IN3, GPIO.LOW)
-        GPIO.output(self.IN4, GPIO.LOW)
-
-    def right(self, sTime=2, speed=30):
-        self.PWMA.ChangeDutyCycle(speed)
-        self.PWMB.ChangeDutyCycle(speed)
-        GPIO.output(self.IN1, GPIO.LOW)
-        GPIO.output(self.IN2, GPIO.HIGH)
-        GPIO.output(self.IN3, GPIO.LOW)
-        GPIO.output(self.IN4, GPIO.HIGH)
-        time.sleep(sTime)
-        self.stop()
-
-    def forward(self, sTime=2, speed=40):
-        self.PWMA.ChangeDutyCycle(speed)
-        self.PWMB.ChangeDutyCycle(speed)
-        GPIO.output(self.IN1, GPIO.LOW)
-        GPIO.output(self.IN2, GPIO.HIGH)
-        GPIO.output(self.IN3, GPIO.HIGH)
-        GPIO.output(self.IN4, GPIO.LOW)
-        time.sleep(sTime)
-        self.stop()
-
-    def backward(self, sTime=2, speed=40):
-        self.PWMA.ChangeDutyCycle(speed)
-        self.PWMB.ChangeDutyCycle(speed)
-        GPIO.output(self.IN1, GPIO.HIGH)
-        GPIO.output(self.IN2, GPIO.LOW)
-        GPIO.output(self.IN3, GPIO.LOW)
-        GPIO.output(self.IN4, GPIO.HIGH)
-        time.sleep(sTime)
-        self.stop()
-
-    def set_pwm_a(self, value):
-        self.PA = value
-        self.PWMA.ChangeDutyCycle(self.PA)
-
-    def set_pwm_b(self, value):
-        self.PB = value
-        self.PWMB.ChangeDutyCycle(self.PB)
-
-    def set_motor(self, left, right):
-        if (right >= 0) and (right <= 100):
-            GPIO.output(self.IN1, GPIO.HIGH)
-            GPIO.output(self.IN2, GPIO.LOW)
-            self.PWMA.ChangeDutyCycle(right)
-        elif (right < 0) and (right >= -100):
-            GPIO.output(self.IN1, GPIO.LOW)
-            GPIO.output(self.IN2, GPIO.HIGH)
-            self.PWMA.ChangeDutyCycle(0 - right)
-        if (left >= 0) and (left <= 100):
-            GPIO.output(self.IN3, GPIO.HIGH)
-            GPIO.output(self.IN4, GPIO.LOW)
-            self.PWMB.ChangeDutyCycle(left)
-        elif (left < 0) and (left >= -100):
-            GPIO.output(self.IN3, GPIO.LOW)
-            GPIO.output(self.IN4, GPIO.HIGH)
-            self.PWMB.ChangeDutyCycle(0 - left)
-
-ab = AlphaBot()
-dtime = 0.5
 
 mDict = {"forward":1,"backward":2, "left":3, "right":4, "stop":5, "fb":6, "zigzag":7, "drift":8}
 
+global failed_login
+failed_login = 0
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
+    global failed_login
     error = None
     if request.method == 'POST':
-        username = request.form['username']
-        #* print(username)
-        password = request.form['password']
-        #* print(password)
-        completion = validate(username, password)
-        if completion == False:
-            error = 'Invalid Credentials. Please try again.'
-            print(error)
-        else:
-            login_log(username)
-            print(f"{username} logged in!")
-            resp = make_response(redirect(url_for('index')))
-            resp.set_cookie('username', username)
-            return resp
+        if failed_login <= 3:
+            username = request.form['username']
+            #* print(username)
+            password = request.form['password']
+            #* print(password)
+            completion = validate(username, password)
+            if completion == False:
+                error = 'Invalid Credentials. Please try again.'
+                failed_login += 1
+                print(error)
+            else:
+                failed_login = 0
+                login_log(username)
+                print(f"{username} logged in!")
+                resp = make_response(redirect(url_for('index')))
+                resp.set_cookie('username', username)
+                return resp
+        elif failed_login > 3 and failed_login < 5:
+            time.sleep(10)
+            username = request.form['username']
+            #* print(username)
+            password = request.form['password']
+            #* print(password)
+            completion = validate(username, password)
+            if completion == False:
+                error = 'Invalid Credentials. Please try again.'
+                failed_login += 1
+                print(error)
+            else:
+                failed_login = 0
+                login_log(username)
+                print(f"{username} logged in!")
+                resp = make_response(redirect(url_for('index')))
+                resp.set_cookie('username', username)
+                return resp
+        elif failed_login >= 5 and failed_login < 15:
+            time.sleep(20)
+            username = request.form['username']
+            #* print(username)
+            password = request.form['password']
+            #* print(password)
+            completion = validate(username, password)
+            if completion == False:
+                error = 'Invalid Credentials. Please try again.'
+                failed_login += 1
+                print(error)
+            else:
+                failed_login = 0
+                login_log(username)
+                print(f"{username} logged in!")
+                resp = make_response(redirect(url_for('index')))
+                resp.set_cookie('username', username)
+                return resp
+        elif failed_login >=15:
+            time.sleep(20)
+            print("Probabile Bruteforce incoming")
+            error = "Page blocked"
     return render_template('login.html', error=error)
 
 
 @app.route(f'/{token}', methods=['GET', 'POST'])
 def index():
-    print("--> Servita pagina di login <--")
+    #print("--> Servita pagina di login <--")
     dbNotFound = False
     connDb = create_connection("Movimenti.db")
     if connDb == None:
